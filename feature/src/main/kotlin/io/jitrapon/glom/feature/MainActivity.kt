ca9f2c86e1c0b74c7ffda9b32c72cd4d3f1f6a38
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2017-11-13 17:45:37 +0700

    Make gradle build for all modules working

--- feature/src/main/kotlin/io/jitrapon/glom/feature/MainActivity.kt
+++ feature/src/main/kotlin/io/jitrapon/glom/feature/MainActivity.kt
@@ -1,7 +1,8 @@
 package io.jitrapon.glom.feature
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
