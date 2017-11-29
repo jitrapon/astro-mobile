2c756192a2492e40bee976c55cc1c852e207e48f
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2017-11-30 03:11:49 +0700

    Remove unused dependency

--- base/src/main/kotlin/io/jitrapon/glom/data/AsyncResult.kt
+++
@@ -1,9 +0,0 @@
-package io.jitrapon.glom.data
-
-/**
- * @author Jitrapon Tiachunpun
- */
-interface AsyncResult<T>
-
-class AsyncSuccessResult<T>(val result: T) : AsyncResult<T>
-class AsyncErrorResult<T>(val error: Throwable) : AsyncResult<T>
