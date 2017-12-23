34241a25d46e4f04f8a138d66affd0f92f2d8d8f
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2017-12-23 14:33:51 +0700

    WIP add recyclervie adapter for showing partial list

---
+++ base/src/main/kotlin/io/jitrapon/glom/base/ui/widget/recyclerview/PartialRecyclerViewAdapter.kt
@@ -0,0 +1,81 @@
+package io.jitrapon.glom.base.ui.widget.recyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+/**
+ * Adapter of the Recyclerview that shows partial number of items from a list.
+ *
+ * Created by Jitrapon
+*/
+abstract class PartialRecyclerViewAdapter<T> : RecyclerView.Adapter<T>() where T : RecyclerView.ViewHolder {
+
+    /**
+     * Child class should return the view type integer to represent the more item type
+     */
+    abstract fun getMoreItemViewType(): Int
+    
+    /**
+     * Child class should return the number of items to be visible
+     */
+    abstract fun getVisibleItemCount(): Int
+
+    /**
+     * Child class should return the number of items originally in the list
+     */
+    abstract fun getAllItemCount(): Int
+
+    /**
+     * Creates a viewholder class that shows remaining number of items
+     */
+    abstract fun onCreateMoreViewHolder(parent: ViewGroup): T
+
+    /**
+     * Creates one or more types of viewholder classes that show the visible items
+     */
+    abstract fun onCreateOtherViewHolder(parent: ViewGroup, viewType: Int): T
+
+    /**
+     * Returns the item view type of the visible list
+     */
+    abstract fun getOtherItemViewType(position: Int): Int
+
+    /**
+     * Child class should override to bind more item
+     */
+    abstract fun onBindMoreItemViewHolder(holder: T, remainingItemCount: Int)
+
+    /**
+     * Child class should override to bind other visible item
+     */
+    abstract fun onBindOtherItemViewHolder(holder: T, position: Int)
+    
+    override fun getItemViewType(position: Int): Int {
+        return if (position == itemCount - 1 && itemCount != getVisibleItemCount()) getMoreItemViewType() 
+        else getOtherItemViewType(position)
+    }
+    
+    override fun getItemCount(): Int {
+        val allCount = getAllItemCount()
+        val visibleCount = getVisibleItemCount()
+        return if (allCount == 0) 0 else {
+            if (visibleCount >= allCount) allCount else visibleCount + 1
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): T {
+        return when (viewType) {
+            getMoreItemViewType() -> onCreateMoreViewHolder(parent)
+            else -> onCreateOtherViewHolder(parent, viewType)
+        }
+    }
+
+    override fun onBindViewHolder(holder: T, position: Int) {
+        if (getItemViewType(position) == getMoreItemViewType()) {
+            onBindMoreItemViewHolder(holder, getAllItemCount() - getVisibleItemCount())
+        }
+        else {
+            onBindOtherItemViewHolder(holder, position)
+        }
+    }
+}
---
+++ board/src/main/kotlin/io/jitrapon/glom/board/AttendeeAdapter.kt
@@ -0,0 +1,43 @@
+package io.jitrapon.glom.board
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import io.jitrapon.glom.base.ui.widget.recyclerview.PartialRecyclerViewAdapter
+
+/**
+ * Created by Jitrapon
+ */
+class AttendeeAdapter : PartialRecyclerViewAdapter<RecyclerView.ViewHolder>() {
+
+    override fun getMoreItemViewType(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getVisibleItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateMoreViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateOtherViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getOtherItemViewType(position: Int): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindMoreItemViewHolder(holder: RecyclerView.ViewHolder, remainingItemCount: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindOtherItemViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
