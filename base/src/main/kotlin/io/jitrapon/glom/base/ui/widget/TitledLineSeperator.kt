6bf7a1c47731845fa2f597a17a16d9e28ccfbba2
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2018-02-27 19:15:02 +0700

    Add line seperator widget

---
+++ base/src/main/kotlin/io/jitrapon/glom/base/ui/widget/TitledLineSeperator.kt
@@ -0,0 +1,44 @@
+package io.jitrapon.glom.base.ui.widget
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import io.jitrapon.glom.R
+
+/**
+ * A line seperator widget that can show title in the center
+ *
+ * @author Jitrapon Tiachunpun
+ */
+class TitledLineSeperator : ConstraintLayout {
+
+    private lateinit var textView: TextView
+
+    constructor(context: Context): super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet? = null) {
+        val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.titled_line_seperator, this)
+
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.TitledLineSeperator)
+            val text = typedArray.getText(R.styleable.TitledLineSeperator_title)
+            textView = findViewById<TextView>(R.id.line_seperator_title).apply {
+                setText(text)
+            }
+            typedArray.recycle()
+        }
+    }
+}
