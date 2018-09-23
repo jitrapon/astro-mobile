9f8cc509197f442acc6b9554d366f7034cb423dc
Jitrapon Tiachunpun <Yoshi3003@gmail.com>
2018-09-23 15:10:39 +0700

    Change board item type query to use Integer type

--- board/src/main/kotlin/io/jitrapon/glom/board/BoardRemoteDataSource.kt
+++ board/src/main/kotlin/io/jitrapon/glom/board/BoardRemoteDataSource.kt
@@ -14,7 +14,7 @@ class BoardRemoteDataSource(private val circleInteractor: CircleInteractor) : Re
     private val api = retrofit.create(BoardApi::class.java)
 
     override fun getBoard(circleId: String, itemType: Int, refresh: Boolean): Flowable<Board> {
-        return api.getBoard(circleId, getTypeQuery(itemType)).map {
+        return api.getBoard(circleId, itemType.toString()).map {
             it.deserialize(circleId)
         }
     }
@@ -33,16 +33,6 @@ class BoardRemoteDataSource(private val circleInteractor: CircleInteractor) : Re
 
     override fun deleteItem(itemId: String, remote: Boolean): Completable {
         return api.deleteBoardItem(circleInteractor.getActiveCircleId(), itemId)
-    }
-
-    private fun getTypeQuery(type: Int): String? = when (type) {
-        BoardItem.TYPE_EVENT -> "event"
-        BoardItem.TYPE_DRAWING -> "drawing"
-        BoardItem.TYPE_FILE -> "file"
-        BoardItem.TYPE_LINK -> "link"
-        BoardItem.TYPE_LIST -> "list"
-        BoardItem.TYPE_NOTE -> "note"
-        else -> null
     }
 
     //region deserializers
